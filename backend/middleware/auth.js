c879704523d46e6376ee007e284f61ecd32dfbe9
Sammed <112275032+SaMmEd8@users.noreply.github.com>
2025-08-29 07:42:30 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -13,7 +13,7 @@ module.exports = function(req, res, next) {
 
     // Verify token
     try {
-        const decoded = jwt.verify(token, 'yourSecretKey'); // Use the same secret key
+        const decoded = jwt.verify(token, 'd3a3c1b1-6b8f-4d32-9c1a-7b8f2d3e4c5a'); // Use the same secret key
         req.user = decoded.user;
         next();
     } catch (err) {
